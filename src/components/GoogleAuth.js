6a0d081bfbc816f2619a0b5ae5418fb5b4f7a9df
Nirav <gusainirav1@gmail.com>
2021-08-04 20:44:33 +0530

    Updating auth state

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,30 @@
 import React, { Component } from "react";
 
 export default class GoogleAuth extends Component {
+  state = { isSingedIn: null };
+
+  //for 1 time rendering
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "1031496197276-nuuh6fjbt1nn71fu9sr6pslpj8kg2thp.apps.googleusercontent.com",
+          scope: "eamil",
+        })
+        .then(() => {
+          // to check whether user is logged in or not?
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSingedIn: this.auth.isSingedIn.get() });
+          this.auth.isSingedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  onAuthChange = () => {
+    this.setState({ isSingedIn: this.auth.isSingedIn.get() });
+  };
+
   render() {
     return <div>GoogleAuth</div>;
   }
