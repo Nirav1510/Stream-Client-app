62b9e8f3f04b890fba16586cde04781d5c1e291c
Nirav <gusainirav1@gmail.com>
2021-08-07 15:27:34 +0530

    created all action-creators

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,7 @@
 export const SIGN_IN = "SIGN_IN";
 export const SIGN_OUT = "SIGN_OUT";
+export const CREATE_STREAM = "CREATE_STREAM";
+export const FETCH_STREAMS = "FETCH_STREAMS"; // all streams
+export const FETCH_STREAM = "FETCH_STREAM"; // single stream
+export const EDIT_STREAM = "EDIT_STREAM";
+export const DELETE_STREAM = "DELETE_STREAM";
