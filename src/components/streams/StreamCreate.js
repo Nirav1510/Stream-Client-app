bfaf246ef3de19af0da8889195a8b9c0c964385e
Nirav <gusainirav1@gmail.com>
2021-08-10 12:53:57 +0530

    refactoring component

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,64 +1,21 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 import { createStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamCreate extends React.Component {
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  }
-
-  renderInput = ({ input, lable, meta }) => {
-    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
-    return (
-      <div className={className}>
-        <label>{lable}</label>
-        <input {...input} autoComplete="off"/>
-        {this.renderError(meta)}
-      </div>
-    );
-  };
-
   onSubmit = (formValues) => {
     this.props.createStream(formValues);
   };
 
   render() {
     return (
-      <form
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form error"
-      >
-        <Field name="title" component={this.renderInput} lable="Enter Title" />
-        <Field
-          name="description"
-          component={this.renderInput}
-          lable="Enter Description"
-        />
-        <button className="ui button primary">Submit</button>
-      </form>
+      <div>
+        <h2>Create a Stream</h2>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
-const validate = (formValues) => {
-  const error = {};
-
-  if (!formValues.title) {
-    error.title = "You must enter a title";
-  }
-  if (!formValues.description) {
-    error.description = "You must enter a description";
-  }
-  return error;
-};
-
-const formWrap = reduxForm({ form: "streamCreate", validate })(StreamCreate);
-
-export default connect(null, { createStream })(formWrap);
+export default connect(null, { createStream })(StreamCreate);
