6d4bb6cdfd0dcc13d16465c9a68d32c127d79a94
Nirav <gusainirav1@gmail.com>
2021-08-12 17:47:49 +0530

    delete stream method implemented correctly

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,24 +1,56 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import Modal from "../Modal";
+import history from "../../history";
+import { fetchStream, deleteStream } from "../../actions";
 
-const StreamDelete = () => {
-  const action = (
-    <div>
-        <button className="ui button negative">Delete</button>
-        <button className="ui button">Cancel</button>
-    </div>
-  )
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
-  return (
-    <div>
-      StreamDelete
-      <Modal 
+  renderAction() {
+    const { id } = this.props.match.params;
+    return (
+      <>
+        <button
+          onClick={() => {
+            this.props.deleteStream(id);
+          }}
+          className="ui button negative"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+      </>
+    );
+  }
+  renderContent() {
+    if (!this.props.stream) {
+      return "Are you sure that you want to delete this stream?";
+    }
+    return `Are you sure that you want to delete the stream with title : ${this.props.stream.title}`;
+  }
+
+  render() {
+    return (
+      <Modal
         title="DELETE STREAM"
-        content="Are you sure that you want to delete this stream?"
-        action={action}
+        content={this.renderContent()}
+        action={this.renderAction()}
+        onDismiss={() => history.push("/")}
       />
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamDelete;
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
